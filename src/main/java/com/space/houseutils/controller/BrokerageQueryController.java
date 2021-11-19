e0848fb8e8167613161006937206c3dd5e26e3b7
Space <enaenen@naver.com>
2021-11-19 11:08:08 +0900

    BrokerageRule -Chap2함수

---
+++ src/main/java/com/space/houseutils/controller/BrokerageQueryController.java
@@ -0,0 +1,20 @@
+package com.space.houseutils.controller;
+
+import com.space.houseutils.constants.ActionType;
+import com.space.houseutils.policy.BrokeragePolicy;
+import com.space.houseutils.policy.BrokeragePolicyFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BrokerageQueryController {
+    @GetMapping("/api/calc/brokerage")
+    public Long calcBrokerage(@RequestParam ActionType actionType,
+                              @RequestParam Long price){
+        // 타입 정의 - 매매 / 임대차
+        //TODO: 중개 수수료 계산하는 로직
+        BrokeragePolicy policy = BrokeragePolicyFactory.of(actionType);
+        return policy.calculate(price);
+    }
+}
---
+++ src/main/java/com/space/houseutils/policy/BrokeragePolicy.java
@@ -0,0 +1,11 @@
+package com.space.houseutils.policy;
+
+public interface BrokeragePolicy {
+
+    BrokerageRule createBrokerageRule (Long price);
+
+    default Long calculate(Long price) {
+        BrokerageRule rule = createBrokerageRule(price);
+        return rule.calcMaxBrokerage(price);
+    }
+}
