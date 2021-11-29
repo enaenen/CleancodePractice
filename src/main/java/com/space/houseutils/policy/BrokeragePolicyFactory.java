95a4854a32d1243475d0ac968b7a84fa9dcf25ef
Space <enaenen@naver.com>
2021-11-30 07:36:49 +0900

    자체 예외처리 생성, 처리

--- src/main/java/com/space/houseutils/policy/BrokeragePolicyFactory.java
+++ src/main/java/com/space/houseutils/policy/BrokeragePolicyFactory.java
@@ -1,6 +1,8 @@
 package com.space.houseutils.policy;
 
 import com.space.houseutils.constants.ActionType;
+import com.space.houseutils.exception.ErrorCode;
+import com.space.houseutils.exception.HouseUtilsException;
 
 public class BrokeragePolicyFactory {
     public static BrokeragePolicy of(ActionType actionType){
@@ -10,7 +12,7 @@ public class BrokeragePolicyFactory {
             case PURCHASE:
                 return new PurchaseBrokeragePolicy();
             default:
-                throw new IllegalArgumentException("해당 ActionType에 대한 정책이 존재하지 않습니다.");
+                throw new HouseUtilsException(ErrorCode.INVALID_REQUEST, "해당 actionType에 대한 정책이 존재하지 않습니다.");
         }
     }
 }
