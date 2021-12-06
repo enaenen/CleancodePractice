a34ecc2f5678638c64215b9206b55bf7c5e927bc
Woo Joo Chae <enaenen@naver.com>
2021-12-07 06:26:35 +0900

    TestCast

---
+++ src/test/java/com/space/houseutils/policy/BrokeragePolicyTest.java
@@ -0,0 +1,45 @@
+package com.space.houseutils.policy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrokeragePolicyTest {
+    PurchaseBrokeragePolicy purchaseBrokeragePolicy;
+    RentBrokeragePolicy rentBrokeragePolicy;
+
+    @BeforeEach
+    public void setup(){
+        purchaseBrokeragePolicy = new PurchaseBrokeragePolicy();
+        rentBrokeragePolicy = new RentBrokeragePolicy();
+    }
+    @Test
+    public void testPurchaseBrokeragePolicy(){
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(30_000_000L), 180_000L);
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(100_000_000L), 500_000L);
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(500_000_000L), 2_000_000L);
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(800_000_000L), 4_000_000L);
+        assertEquals(
+                purchaseBrokeragePolicy.calculate(1_000_000_000L), 9_000_000L);
+
+    }
+
+    @Test
+    public void testRentBrokeragePolicy(){
+        assertEquals(
+                rentBrokeragePolicy.calculate(30_000_000L), 150_000L);
+        assertEquals(
+                rentBrokeragePolicy.calculate(100_000_000L), 300_000L);
+        assertEquals(
+                rentBrokeragePolicy.calculate(500_000_000L), 2_000_000L);
+        assertEquals(
+                rentBrokeragePolicy.calculate(800_000_000L), 6_400_000L);
+        assertEquals(
+                rentBrokeragePolicy.calculate(1_000_000_000L), 8_000_000L);
+    }
+}
